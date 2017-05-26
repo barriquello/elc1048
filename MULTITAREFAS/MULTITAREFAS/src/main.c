182f52907a236d9107d2fbb3a42bec65faa516e4
barriquello <barriquello@gmail.com>
2017-05-26 11:02:17 -0300

    Sistema multitarefas v0.1

---
+++ MULTITAREFAS/MULTITAREFAS/src/main.c
@@ -0,0 +1,123 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * This is a bare minimum user application template.
+ *
+ * For documentation of the board, go \ref group_common_boards "here" for a link
+ * to the board-specific documentation.
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# Minimal main function that starts with a call to system_init()
+ * -# Basic usage of on-board LED and button
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
+#include <asf.h>
+#include "stdint.h"
+#include "multitarefas.h"
+
+void tarefa_1(void);
+void tarefa_2(void);
+void tarefa_3(void);
+void tarefa_4(void);
+
+#define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
+
+uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
+uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
+uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
+
+int main(void)
+{
+	system_init();
+	
+	/* Criação das tarefas */
+	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
+	
+	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+	
+	/* Inicia sistema multitarefas */
+	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
+	
+	//ConfiguraMarcaTempo();   /* Configura marca de tempo */
+	
+	IniciaMultitarefas();
+	
+	/* Nunca entra aqui */
+	while (1)
+	{
+	}
+}
+
+/* exemplo de funções para suspender/continuar as tarefas */
+void tarefa_1(void)
+{
+	volatile uint16_t a = 0;
+	for(;;)
+	{
+		a++;
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Turn LED on. */
+		TarefaContinua(2);
+	}
+}
+
+void tarefa_2(void)
+{
+	volatile uint16_t b = 0;
+	for(;;)
+	{
+		b++;
+		TarefaContinua(1);
+		TarefaSuspende(2);	
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
+	}
+}
+
+/* exemplo de funções para suspender as tarefas por algum tempo (atraso/delay) */
+void tarefa_3(void)
+{
+	volatile uint16_t a = 0;
+	for(;;)
+	{
+		a++;	
+			
+		/* Turn LED on. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(3); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Turn LED off. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(3); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+	}
+}
+
+void tarefa_4(void)
+{
+	volatile uint16_t b = 0;
+	for(;;)
+	{
+		b++;
+		TarefaEspera(5);	/* tarefa 2 se coloca em espera por 5 marcas de tempo (ticks) */
+	}
+}
